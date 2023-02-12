d00bc22cf456277ab1bdbbcdf634ef90d0c30cc1
mostafa-kamal <mmkamal021@gmail.com>
2023-02-12 13:23:57 +0600

    commited Destructuring-Array

---
+++ Destructuring(Array).js
@@ -0,0 +1,22 @@
+// faster/easier way to access/unpack variables from arrays, objects (later videos)
+
+const fruits = ['orange', 'banana', 'lemon']
+const friends = ['jhon', 'peter', 'bob', 'anna', 'kelly']
+
+const fruit1 = fruits[0]
+const fruit2 = fruits[1]
+const fruit3 = fruits[2]
+
+console.log(fruit1, fruit2, fruit3)
+
+const [enemy, , bob, , kelly, susan] = friends
+console.log(enemy, bob, kelly, susan)
+
+let first = 'bob'
+let second = 'john'
+// let temp = second
+// second = first
+// first = temp
+
+;[second, first] = [first, second]
+console.log(first, second)
