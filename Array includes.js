d9e661bbffa22abb830f78cf37d53f991b5b87fb
mostafa-kamal <mmkamal021@gmail.com>
2023-02-14 20:12:06 +0600

    commited Array includes

---
+++ Array includes.js
@@ -0,0 +1,15 @@
+// Array includes() - checks the item is an array
+// useful in the conditional statements
+
+const groceries = ['milk', 'bread', 'meat']
+
+let rendomItem = 'lemon'
+randomItem = 'milk'
+
+// const isIncluded = groceries.includes(randomItem)
+// const isIncluded = groceries.includes(randomItem, 1)
+// console.log(isIncluded)
+
+if (groceries.includes(randomItem)) {
+  console.log(`Yeah! it's on the list`)
+}
