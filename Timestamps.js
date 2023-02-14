737785c0621b7aa614f83ff12bc1e0f1a5adc8f2
mostafa-kamal <mmkamal021@gmail.com>
2023-02-14 13:08:29 +0600

    commited Timestamps (unsloved)

---
+++ Timestamps.js
@@ -0,0 +1,22 @@
+// Timestamps
+console.log(new Date()) // Mon Sep 27 2021 09:42:13 GMT-0700 (Pacific Daylight Time)
+
+// Unix Time
+// January 1, 1970
+// 1s = 1000ms
+
+// Date.now()
+console.log(Date.now())
+// new Date().getTime()
+console.log(new Date().getTime())
+// new Date().valueOf()
+console.log(new Date().valueOf())
+
+setTimeout(() => {
+  console.log(Date.now())
+}, 1000)
+
+// create id's in learning apps
+
+// create/get dates
+// 01979985368
